34fc0851341d09e728b721fcbec189078e6331ae
Narongdech Soontornekajit <103498705@student.swin.edu.au>
2023-10-16 22:03:57 +1100

    Update history, buy pages

--- src/buy.js
+++ src/buy.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios'; // Import axios
 import "./buy.css";
 
 function Buy() {
@@ -13,12 +14,35 @@ function Buy() {
   const transactionHash = searchParams.get('transactionHash');
   const timestamp = searchParams.get('timestamp');
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleProceed = () => {
+    // Create query parameters for the GET request
+    const queryParams = `?product_id=${productId}&product_name=${productName}&product_description=${productDesc}&price=${price}&user_id=1&transaction_hash=${transactionHash}&timestamp=${timestamp}`;
+
+    // Send a GET request with the query parameters
+    setIsLoading(true); // You can set loading state here
+    axios.get(`http://127.0.0.1:4000/insert_history${queryParams}`)
+      .then(response => {
+        console.log('Transaction data retrieved successfully:', response.data);
+        setIsLoading(false); // Set loading state back to false
+        // You can handle the response here, but it won't navigate to a new URL
+      })
+      .catch(error => {
+        console.error('Error retrieving transaction data:', error);
+        setIsLoading(false); // Set loading state back to false
+      });
+
+    // Navigate to the '/history' page
+    navigate('/history');
+  };
+
   return (
     <div className="transaction-tile">
       {/* Display product details */}
       <h2>Transaction</h2>
       <p>Transaction Hash: {transactionHash}</p>
-      <p>Timestamp: {timestamp}</p> {/* Display the timestamp */}
+      <p>Timestamp: {timestamp}</p>
       <p>Product ID: {productId}</p>
       <p>Price Name: {productName}</p>
       <p>Product Disc: {productDesc}</p>
@@ -26,7 +50,11 @@ function Buy() {
       <p>Status: Success</p>
       <p>Type: Buy</p>
 
-      <button className="proceed-button" onClick={() => navigate('/history')}>Proceed</button>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <button className="proceed-button" onClick={handleProceed}>Proceed</button>
+      )}
     </div>
   );
 }
--- src/history.js
+++ src/history.js
@@ -1,69 +1,53 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import './history.css'; // Import the grid.css file
-import { Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import './history.css';
+import axios from 'axios';
 
+export default function HistoryPage() {
+  const [transactions, setTransactions] = useState([]);
 
-const transactions = [
-  {
-    itemName: 'Product A',
-    itemDescription: 'Description of Product A',
-    time: '10:30 AM',
-    price: '$100',
-  },
-  {
-    itemName: 'Product B',
-    itemDescription: 'Description of Product B',
-    time: '11:45 AM',
-    price: '$50',
-  },
-  {
-    itemName: 'Product C',
-    itemDescription: 'Description of Product C',
-    time: '1:15 PM',
-    price: '$75',
-  },
-  // Add more transactions as needed
-];
+  useEffect(() => {
+    // Hard-code the user ID as 1
+    const userId = 1;
 
-export default function BasicGrid() {
+    // Fetch transaction history data from the history table using the user ID
+    axios.get(`http://127.0.0.1:4000/history?user_id=${userId}`)
+      .then((response) => {
+        setTransactions(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <Box  
-      className="history-container"
-      sx={{
-        p: 2,
-        marginTop: 5,
-        paddingBottom: 10,
-        bgcolor: '#ffffff76',
-        borderRadius: 5,
-      }}
-    >
-      <Grid container>
-        <Grid xs={12} paddingBottom={6}><h2 className="history-heading">History</h2></Grid>
-        
-        {transactions.map((transaction, index) => (
-          <Grid 
-            item xs={12} sm={12} md={6}
-            className="transaction-row" 
-            key={index}
-            sx={{
-              p:2
-            }}
-          >
-            <div className="transaction-details">
-              <div className="item-name">{transaction.itemName}</div>
-              <div className="item-description">{transaction.itemDescription}</div>
-              <div className="time-price">
-                Time: {transaction.time} | Price: {transaction.price}
-              </div>
-              <div><hr className='line-break'></hr></div>
+    <div>
+      <h1>History</h1>
+      {transactions.map((transaction) => (
+        <Paper key={transaction.product_id} className="product-tile">
+          <div className="product-info">
+            <Typography variant="body2" color="text.secondary" style={{ color: 'black' }}>
+              Product ID: {transaction.product_id}
+            <Typography className="product-name" variant="subtitle1" style={{ color: 'black' }}>
+              Product Name: {transaction.product_name}
+            </Typography>
+            <Typography className="product-description" variant="body2" style={{ color: 'black' }}>
+              Product Description: {transaction.product_description}
+            </Typography>
+            </Typography>
+            <Typography className="product-price" variant="subtitle1" style={{ color: 'black' }}>
+              Price: ${transaction.price}
+            </Typography>
+          </div>
+          <div className="transaction-details">
+            <div className="time-price" style={{ color: 'black' }}>
+              <span className="with-underline">Time:</span><br />{transaction.timestamp} <br /><span className="with-underline">Transaction Hash:</span> <span class="transaction-hash">{transaction.transaction_hash}</span>
             </div>
-            
-          </Grid>
-          
-        ))}
-        
-      </Grid>
-    </Box>
+          </div>
+        </Paper>
+      ))}
+    </div>
   );
+  
 }
